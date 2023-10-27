6bc65b978f78944defad02bf2a8970d624d50883
Josep F. Hervás Fuertes <josep.hervas@gmail.com>
2023-10-27 11:16:29 +0200

    Code refactor

--- mediaserver/mediaserver.js
+++ mediaserver/mediaserver.js
@@ -1,11 +1,11 @@
-import http from "http";
-import url from "url";
-import yt from "youtube-search-without-api-key";
-import spawn from "child_process";
-import fs from "fs";
-import sanitize from "sanitize-filename";
+const http = require("http");
+const url = require('url');
+const yt = require('youtube-search-without-api-key');
+const { spawn } = require('node:child_process');
+const fs = require('fs');
+var sanitize = require("sanitize-filename");
 
-const DEBUG = false;
+const DEBUG = true;
 
 const htmlPath = '/var/www/html'
 const mediaServerPath = '/usr/local/mediaserver'
@@ -23,6 +23,7 @@ async function youtubeSearch(queryObject, res, req, action) {
     DEBUG && console.log(queryObject);
 
     const searchResults = await yt.search(searchText);
+
     if (DEBUG) {
         console.log(`Search text: ${searchText}`);
         console.log(searchResults);
@@ -35,7 +36,8 @@ async function youtubeSearch(queryObject, res, req, action) {
         return;
     }
 
-    videos = searchResults.filter(video => video.id.videoId === id);
+    const videos = searchResults.filter(video => video.id.videoId === id);
+
     if (videos.length) {
         DEBUG && console.log(videos[0]);
 
@@ -43,27 +45,26 @@ async function youtubeSearch(queryObject, res, req, action) {
         const url_yt = videos[0].url;
         const filename = sanitize(title).replace(/['\(\)\[\]\&#]/g, "");
         const uri = encodeURI(filename);
-        const fullpath = htmlPath + '/' + filename;
-
-        format = type.localeCompare('video') ? format = 'bestaudio' : 'bestvideo[ext=webm]+bestaudio[ext=webm]/best[ext=webm]/best';
-        outputFormat = queryObject.output_format ? queryObject.output_format : 'webm';
-        format = 'bestvideo[width<=1920][ext=' + outputFormat + ']+bestaudio[ext=' + outputFormat + ']/best[width<=1920][ext=' + outputFormat + ']/best'
-        params_youtube_dl = ['-f', format, '--merge-output-format', outputFormat, '--prefer-ffmpeg'];
+        const fullPath = htmlPath + '/' + filename;
+        const outputFormat = queryObject.output_format ? queryObject.output_format : 'webm';
+        const format = 'bestvideo[width<=1920][ext=' + outputFormat + ']+bestaudio[ext=' + outputFormat + ']/best[width<=1920][ext=' + outputFormat + ']/best'
+        let params_youtube_dl = ['-f', format, '--merge-output-format', outputFormat, '--prefer-ffmpeg'];
         const parameters = new URLSearchParams('filename=' + uri + '&type=' + type + '&text=' + searchText + '&id=' + id);
+
         if (restart) {
-            if (fs.existsSync(fullpath + '.log'))
-                fs.unlinkSync(fullpath + '.log');
-            if (fs.existsSync(fullpath + '.err'))
-                fs.unlinkSync(fullpath + '.err');
-            if (fs.existsSync(fullpath + '.webm'))
-                fs.unlinkSync(fullpath + '.webm');
-            if (fs.existsSync(fullpath + '.mp4'))
-                fs.unlinkSync(fullpath + '.mp4');
-            if (fs.existsSync(fullpath + '.mp3'))
-                fs.unlinkSync(fullpath + '.mp3');
+            if (fs.existsSync(fullPath + '.log'))
+                fs.unlinkSync(fullPath + '.log');
+            if (fs.existsSync(fullPath + '.err'))
+                fs.unlinkSync(fullPath + '.err');
+            if (fs.existsSync(fullPath + '.webm'))
+                fs.unlinkSync(fullPath + '.webm');
+            if (fs.existsSync(fullPath + '.mp4'))
+                fs.unlinkSync(fullPath + '.mp4');
+            if (fs.existsSync(fullPath + '.mp3'))
+                fs.unlinkSync(fullPath + '.mp3');
         }
 
-        if (fs.existsSync(fullpath + '.log')) {
+        if (fs.existsSync(fullPath + '.log')) {
             res.writeHead(302, {
                 'Location': urlPrefix + '/waitingroom.php?' + parameters
             });
@@ -73,10 +74,10 @@ async function youtubeSearch(queryObject, res, req, action) {
 
         if (!type.localeCompare('video')) {
 
-            if (fs.existsSync(fullpath + '.mp4')) {
+            if (fs.existsSync(fullPath + '.mp4')) {
                 outputFormat = 'mp4';
-                if (fs.existsSync(fullpath + '.webm')) {
-                    fs.unlinkSync(fullpath + '.webm')
+                if (fs.existsSync(fullPath + '.webm')) {
+                    fs.unlinkSync(fullPath + '.webm')
                 }
                 res.writeHead(302, {
                     'Location': urlPrefix + '/player.php?filename=' + uri + '.mp4'
@@ -84,7 +85,7 @@ async function youtubeSearch(queryObject, res, req, action) {
                 res.end();
                 return;
             } else {
-                if (fs.existsSync(fullpath + '.webm')) {
+                if (fs.existsSync(fullPath + '.webm')) {
                     res.writeHead(302, {
                         'Location': urlPrefix + '/player.php?filename=' + uri + '.webm'
                     });
@@ -93,7 +94,7 @@ async function youtubeSearch(queryObject, res, req, action) {
                 }
             }
         } else {
-            if (fs.existsSync(fullpath + '.mp3')) {
+            if (fs.existsSync(fullPath + '.mp3')) {
                 res.writeHead(302, {
                     'Location': urlPrefix + '/' + uri + '.mp3'
                 });
@@ -102,27 +103,27 @@ async function youtubeSearch(queryObject, res, req, action) {
             }
         }
 
-        fs.appendFileSync(fullpath + '.log', '[download] 0% ETA 99:99');
+        fs.appendFileSync(fullPath + '.log', '[download] 0% ETA 99:99');
 
-        params_youtube_dl.push('--socket-timeout', '10', '--retries', 'infinite', '--output', (fullpath + '.%(ext)s'), url_yt);
+        params_youtube_dl.push('--socket-timeout', '10', '--retries', 'infinite', '--output', (fullPath + '.%(ext)s'), url_yt);
         console.log('youtube-dl');
         console.log(params_youtube_dl);
         const youtube_dl = spawn(mediaServerPath + "/youtube-dl", params_youtube_dl, { detached: true, stdio: ['pipe', 'pipe', 'pipe'] });
 
         youtube_dl.stdout.on('data', function(chunk) {
-            if (chunk) fs.appendFileSync(fullpath + '.log', chunk);
+            if (chunk) fs.appendFileSync(fullPath + '.log', chunk);
         });
         youtube_dl.stderr.on('data', function(chunk) {
-            if (chunk) fs.appendFileSync(fullpath + '.err', chunk);
+            if (chunk) fs.appendFileSync(fullPath + '.err', chunk);
         });
         youtube_dl.on('close', function(code, signal) {
-            if (fs.existsSync(fullpath + '.log')) fs.unlinkSync(fullpath + '.log');
-            if (!fs.existsSync(path + 'transcription/' + filename + '.vtt')) {
-                if (fs.existsSync(fullpath + '.webm')) {
-                    const whisper_ctranslate2 = spawn("/usr/local/bin/whisper-ctranslate2", ['--model', 'small', '--print_colors', 'True', '--task', 'transcribe', fullpath + '.webm', '--output_format', 'vtt', '--vad_filter', 'True', '--output_dir', htmlPath + '/transcription/', '--compute_type', 'int8', '--threads', '"$(cat /proc/cpuinfo | grep "processor" | wc -l)"'], { detached: true, stdio: ['pipe', 'pipe', 'pipe'] });
+            if (fs.existsSync(fullPath + '.log')) fs.unlinkSync(fullPath + '.log');
+            if (!fs.existsSync(mediaServerPath + 'transcription/' + filename + '.vtt')) {
+                if (fs.existsSync(fullPath + '.webm')) {
+                    const whisper_ctranslate2 = spawn("/usr/local/bin/whisper-ctranslate2", ['--model', 'small', '--print_colors', 'True', '--task', 'transcribe', fullPath + '.webm', '--output_format', 'vtt', '--vad_filter', 'True', '--output_dir', htmlPath + '/transcription/', '--compute_type', 'int8', '--threads', '"$(cat /proc/cpuinfo | grep "processor" | wc -l)"'], { detached: true, stdio: ['pipe', 'pipe', 'pipe'] });
                 }
-                else if (fs.existsSync(fullpath + '.mp4')) {
-                    const whisper_ctranslate2 = spawn("/usr/local/bin/whisper-ctranslate2", ['--model', 'small', '--print_colors', 'True', '--task', 'transcribe', fullpath + '.mp4', '--output_format', 'vtt', '--vad_filter', 'True', '--output_dir', htmlPath + '/transcription/', '--compute_type', 'int8', '--threads', '"$(cat /proc/cpuinfo | grep "processor" | wc -l)"'], { detached: true, stdio: ['pipe', 'pipe', 'pipe'] });
+                else if (fs.existsSync(fullPath + '.mp4')) {
+                    const whisper_ctranslate2 = spawn("/usr/local/bin/whisper-ctranslate2", ['--model', 'small', '--print_colors', 'True', '--task', 'transcribe', fullPath + '.mp4', '--output_format', 'vtt', '--vad_filter', 'True', '--output_dir', htmlPath + '/transcription/', '--compute_type', 'int8', '--threads', '"$(cat /proc/cpuinfo | grep "processor" | wc -l)"'], { detached: true, stdio: ['pipe', 'pipe', 'pipe'] });
                 }
             }
             /*
@@ -130,11 +131,11 @@ async function youtubeSearch(queryObject, res, req, action) {
              */
             /*
             if (!fs.existsSync(path + 'translation/' + filename + '.vtt')) {
-                if (fs.existsSync(fullpath + '.webm')) {
-                    const whisper_ctranslate2 = spawn("/usr/local/bin/whisper-ctranslate2", ['--model', 'small', '--print_colors', 'True', '--task', 'translate', fullpath + '.webm', '--output_format', 'vtt', '--vad_filter', 'True', '--output_dir', htmlPath + '/translation/', '--compute_type', 'int8', '--threads', '1'], { detached: true, stdio: ['pipe', 'pipe', 'pipe'] });
+                if (fs.existsSync(fullPath + '.webm')) {
+                    const whisper_ctranslate2 = spawn("/usr/local/bin/whisper-ctranslate2", ['--model', 'small', '--print_colors', 'True', '--task', 'translate', fullPath + '.webm', '--output_format', 'vtt', '--vad_filter', 'True', '--output_dir', htmlPath + '/translation/', '--compute_type', 'int8', '--threads', '1'], { detached: true, stdio: ['pipe', 'pipe', 'pipe'] });
                 }
-                else if (fs.existsSync(fullpath + '.mp4')) {
-                    const whisper_ctranslate2 = spawn("/usr/local/bin/whisper-ctranslate2", ['--model', 'small', '--print_colors', 'True', '--task', 'translate', fullpath + '.mp4', '--output_format', 'vtt', '--vad_filter', 'True', '--output_dir',  htmlPath + '/translation/', '--compute_type', 'int8', '--threads', '1'], { detached: true, stdio: ['pipe', 'pipe', 'pipe'] });
+                else if (fs.existsSync(fullPath + '.mp4')) {
+                    const whisper_ctranslate2 = spawn("/usr/local/bin/whisper-ctranslate2", ['--model', 'small', '--print_colors', 'True', '--task', 'translate', fullPath + '.mp4', '--output_format', 'vtt', '--vad_filter', 'True', '--output_dir',  htmlPath + '/translation/', '--compute_type', 'int8', '--threads', '1'], { detached: true, stdio: ['pipe', 'pipe', 'pipe'] });
                 }
             }
             */
@@ -148,11 +149,12 @@ async function youtubeSearch(queryObject, res, req, action) {
 }
 
 http.createServer(function(req, res) {
-    const request_url = url.parse(req.url, true);
-    console.log('Request URL: ' + request_url);
-    const action = /[^/]+/.exec(request_url.pathname).toString();
-    console.log('Action: ' + action);
-    const queryObject = url.parse(request_url, true).query;
+    const requestURL = url.parse(req.url, true);
+    const action = /[^/]+/.exec(requestURL.pathname).toString();
+    const queryObject = url.parse(requestURL, true).query;
+
+    DEBUG && console.log('Request URL: ' + requestURL);
+    DEBUG && console.log('Action: ' + action);
 
     if (queryObject.text) {
         youtubeSearch(queryObject, res, req, action);
